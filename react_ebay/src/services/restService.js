4067de28203f7010ed7ba93e2bf9938db9d11b4b
albeany <atabernerovalle@gmail.com>
2024-10-23 13:10:27 +0200

    añadido formik y yup a Registro.js

--- react_ebay/src/services/restService.js
+++ react_ebay/src/services/restService.js
@@ -1,6 +1,10 @@
-
 let restService = {
-
+  RegistrarCliente: function (datosCuenta) {
+    return fetch("http//localhost:3003/cliente/Registro", {
+      method: "POST",
+      body: JSON.stringify({ datosCuenta }),
+    });
+  }
 };
 
-export default restService
+export default restService;
